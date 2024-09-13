f481daa3f85c24dc48d6c2a2383ce977989ca81f
nguyenphuonguyentran <trannguyenphuonguyen04072003@gmail.com>
2024-09-13 13:09:11 -0600

    Commit Assignment 2 - Part 2
    Create week-2 page, create StudentInfo component, render StudentInfo in week-2 page, and edit root page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p> Nguyen Phuong Uyen Tran </p>
+      <Link href="https://github.com/nguyenphuonguyentran/cprg306-assignments.git">
+        GitHub repository
+      </Link>
+    </div>
+  );
+}
