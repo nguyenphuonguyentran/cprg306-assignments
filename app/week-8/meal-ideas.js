ba6cc023a76982732df2a0bd0fd9f78b93fbf47c
nguyenphuonguyentran <trannguyenphuonguyen04072003@gmail.com>
2024-10-29 09:44:30 -0600

    Assignment 8 - Fixing Errors

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -15,8 +15,13 @@ export default function MealIdeas({ ingredient }) {
   }, [ingredient]);
 
   return (
-    <div>
-      <h1> Meal Ideas </h1>
+    <div className="text-slate-700">
+      <div className="flex font-semibold">
+        <h1 className="p-2 ml-10 text-4xl text-transparent font-bold font-sans bg-clip-text bg-gradient-to-r from-slate-400 to-slate-700 justify-center items-center flex">
+          {" "}
+          Meal Ideas{" "}
+        </h1>
+      </div>
       <ul>
         {meals.map((meal) => (
           <li key={meal.idMeal}>{meal.strMeal}</li>
--- app/week-8/page.js
+++ app/week-8/page.js
@@ -15,23 +15,30 @@ export default function Page() {
   };
 
   const handleItemSelect = (item) => {
-    const cleanedName = item.name.replace(
+    console.log(item);
+    const cleanedName = item.replace(
       /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
       ""
     );
 
-    setSelectedItemName(cleanedName.split(",").trim());
+    setSelectedItemName(cleanedName.split(",")[0].trim());
   };
 
   return (
-    <main className="bg-center bg-cover bg-gradient-to-r from-cyan-700 to-violet-200">
-      <h1 className="p-5 text-7xl text-transparent font-bold font-sans bg-clip-text bg-gradient-to-r from-slate-200 to-white justify-center items-center flex">
-        Shopping List
-      </h1>
-      <div className="flex flex-col justify-center items-center">
-        <NewItem onAddItem={handleAddItem} />
-        <ItemList items={items} onItemSelect={handleItemSelect} />
-        <MealIdeas ingredient={selectedItemName} />
+    <main className="flex bg-center bg-cover bg-gradient-to-r from-cyan-700 to-violet-200">
+      <div className="flex w-full">
+        <div className="w-1/2 pr-4">
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="p-5 text-7xl text-transparent font-bold font-sans bg-clip-text bg-gradient-to-r from-slate-200 to-white justify-center items-center flex">
+              Shopping List
+            </h1>
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList items={items} onItemSelect={handleItemSelect} />
+          </div>
+        </div>
+        <div className="w-1/2 pl-4 mt-40 bg-white w-80 h-fit rounded-md">
+          <MealIdeas ingredient={selectedItemName} />
+        </div>
       </div>
     </main>
   );
